bee64e926433b29e5bd598e62ed68e4f6965f4ce
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2024-02-27 18:10:51 +0400

    ready project

--- src/components/UI/SaveButton.jsx
+++
@@ -1,5 +0,0 @@
-export const SaveButton = () => {
-    return (
-        <button className="border rounded-xl bg-purple-700 text-white px-4 py-2 mt-4">Save to Spotify</button>
-    )
-}
--- src/components/UI/SearchButton.jsx
+++
@@ -1,5 +0,0 @@
-export const SearchButton = ({handleClick})=>{
-    return (
-        <button onClick={handleClick} className="border rounded-xl bg-purple-700 text-white px-4 py-2 mt-4">Search</button>
-    )
-}
